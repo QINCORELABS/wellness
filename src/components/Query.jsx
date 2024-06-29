5bb9b218025a2c0651a083ff5bf19054e1687620
rahuldileep123 <rahuldileep997@gmail.com>
2024-06-29 08:18:47 +0530

    version 2

--- src/components/Query.jsx
+++ src/components/Query.jsx
@@ -5,16 +5,17 @@ function Query() {
     <>
     <div id='book' style={{backgroundColor:"#C1FFF8"}} className="container-fluid p-5">
         <div className="row">
-            <div className="col-lg-6 d-flex align-items-center justify-content-center">
-               <img width={"50%"} src={Qry} alt="" />
+            <div className="col-lg-6 d-flex align-items-center justify-content-center mb-4">
+               <img width={"45%"} src={Qry} alt="" />
             </div>
-            <div className="col-lg-6">
+            <div className="col-lg-6 d-flex align-items-center justify-content-center">
                 <div>
-                    <h1>Quick Enquery</h1>
-                    <input type="text" className='form-control mb-2' placeholder='Your Name' />
-                    <input type="email" className='form-control mb-2' placeholder='Email Address' />
-                    <input type="text" className='form-control mb-2' placeholder='subject' />
-                    <textarea type="text-area" className='form-control mb-2' placeholder='your messege' row="3" />
+                    <h1 className='mb-2'>Quick Enquery</h1>
+                    <input type="text" className='p-1 w-100 border-black border-2 border-top-0 border-end-0 border-start-0 mb-2 bg-transparent' placeholder='Your Name' />
+                    <input type="email" className='p-1 w-100 border-black border-2 border-top-0 border-end-0 border-start-0 mb-2 bg-transparent' placeholder='Email Address' />
+                    <input type="text" className='p-1 w-100 border-black border-2 border-top-0 border-end-0 border-start-0 mb-2 bg-transparent' placeholder='subject' />
+                    <textarea type="text-area" className='p-1 w-100 border-black border-1  mb-2 bg-transparent' placeholder='your messege' row="4" />
+                    <button className='btn btn-primary w-100 '>submit</button>
                     </div>
             </div>
         </div>
